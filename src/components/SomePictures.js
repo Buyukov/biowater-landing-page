3713605c9dd6ffb8bada5b44237b1c7d00a84c1f
Buyukov <dilmurod.buyukovv@gmail.com>
2023-11-23 23:01:49 +0500

    Update SomePic

--- src/components/SomePictures.js
+++ src/components/SomePictures.js
@@ -8,55 +8,55 @@ export default function SomePictures() {
       <Row>
         <Col sm={6}>
           <div className="somePicItems">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <img
-                    src="https://egesuaritmasistemleri.com/tema/genel/uploads/hakkimiza2-1.jpg"
-                    alt="Avatar"
-                    className="somePicImgStyle"
-                  />
-                </div>
-                <div class="flip-card-back">
-                  <img
-                    // className="padding-7rem"
-                    src={WebLogo}
-                    width="100%"
-                    height="100%"
-                    alt="AboutUsImg"
-                    fluid
-                    rounded
-                  />
-                </div>
-              </div>
-            </div>
+            {/* <div class="flip-card"> */}
+            {/* <div class="flip-card-inner"> */}
+            {/* <div class="flip-card-front"> */}
+            <img
+              src="https://egesuaritmasistemleri.com/tema/genel/uploads/hakkimiza2-1.jpg"
+              alt="Avatar"
+              className="somePicImgStyle"
+            />
           </div>
+          {/* <div class="flip-card-back"> */}
+          {/* <img
+            // className="padding-7rem"
+            src={WebLogo}
+            width="100%"
+            height="100%"
+            alt="AboutUsImg"
+            fluid
+            rounded
+          /> */}
+          {/* </div> */}
+          {/* </div> */}
+          {/* </div> */}
+          {/* </div> */}
         </Col>
         <Col sm={6}>
           <div className="somePicItems">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <img
-                    src="https://egesuaritmasistemleri.com/tema/genel/uploads/hakkimiza2-2.jpg"
-                    alt="Avatar"
-                    className="somePicImgStyle"
-                  />
-                </div>
-                <div class="flip-card-back">
-                  <img
-                    // className="padding-7rem"
-                    src={WebLogo}
-                    width="100%"
-                    height="100%"
-                    alt="AboutUsImg"
-                    fluid
-                    rounded
-                  />
-                </div>
-              </div>
-            </div>
+            {/* <div class="flip-card"> */}
+            {/* <div class="flip-card-inner"> */}
+            {/* <div class="flip-card-front"> */}
+            <img
+              src="https://egesuaritmasistemleri.com/tema/genel/uploads/hakkimiza2-2.jpg"
+              alt="Avatar"
+              className="somePicImgStyle"
+            />
           </div>
+          {/* <div class="flip-card-back">
+            <img
+              // className="padding-7rem"
+              src={WebLogo}
+              width="100%"
+              height="100%"
+              alt="AboutUsImg"
+              fluid
+              rounded
+            />
+          </div> */}
+          {/* </div> */}
+          {/* </div> */}
+          {/* </div> */}
         </Col>
       </Row>
     </Container>
